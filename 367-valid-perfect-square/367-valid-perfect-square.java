a45337ab13cae7c0ad804dbac902f1fd5f4dc6be
Eshaan Dabasiya <76681468+im3dabasia@users.noreply.github.com>
2022-08-17 21:24:13 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (52.58%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.java
+++ 367-valid-perfect-square/367-valid-perfect-square.java
@@ -1,10 +1,36 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
         
-        int ans = (int) Math.pow(num,0.5);
+//         int ans = (int) Math.pow(num,0.5);
         
-        if ( ans * ans == num){
-            return true;
+//         if ( ans * ans == num){
+//             return true;
+//         }
+//         return false;
+        
+        long start = 0;
+        long end = num;
+        long mid = start + ((end - start)/2);
+        
+        while(start <= end){
+            
+            mid = start + ((end - start)/2);
+            
+            if((mid * mid) == num){
+                
+                return true;
+            
+            }
+            else if((mid * mid) < num){
+                
+                start = mid + 1;
+            
+            }else if((mid * mid) > num){
+                
+                end = mid - 1  ;
+            
+            }
+            
         }
         return false;
         
