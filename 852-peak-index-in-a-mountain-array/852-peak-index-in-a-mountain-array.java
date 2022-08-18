197accaa43a9d6003e8d2abf172ef756e92097a7
Eshaan Dabasiya <76681468+im3dabasia@users.noreply.github.com>
2022-08-18 09:54:44 +0530

    Time: 0 ms (100.00%), Space: 73.9 MB (11.00%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -3,13 +3,31 @@ class Solution {
         
         //Solution 1 can be a linear scan with 0(n)
         
-        int i = 0;
+//         int i = 0;
         
-        while(arr[i] < arr[i + 1]){
-            i++;
+//         while(arr[i] < arr[i + 1]){
+//             i++;
+//         }
+//         return i;
+        
+        int start = 0;
+        int end = arr.length -1;
+        
+        while(start < end){
+            
+            int mid = start + ((end -start)/2);
+            
+            if(arr[mid] > arr[mid + 1]){
+                
+                
+                end = mid;
+            }
+            else{
+                
+                start = mid + 1;
+            }
         }
-        return i;
-        
+        return end;
         
     }
 }
