68abd05be724879eca49559e30dff8ff1198c2f3
Eshaan Dabasiya <76681468+im3dabasia@users.noreply.github.com>
2022-10-05 17:58:23 +0530

    Time: 1 ms (83.49%), Space: 50.3 MB (84.00%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,21 +1,40 @@
 class Solution {
     public int missingNumber(int[] nums) {
         
-        int[] hashMap = new int[nums.length + 1];
+//         Approach 2 Xor approach
         
-        for(int i = 0; i < nums.length; i++){
+        int Array_XOR = nums[0];
+        
+        for(int i = 1; i < nums.length; i++){
             
-            hashMap[nums[i]]++;
-            
+            Array_XOR = Array_XOR ^ nums[i];
         }
         
-        for(int i = 1; i < hashMap.length; i++){
+        for(int i = 0; i <= nums.length; i++){
             
-            if(hashMap[i] == 0){
-                return i;
-            }
+            Array_XOR = Array_XOR ^ i;
         }
+        return Array_XOR;
         
-        return 0;
+        
+        
+        
+//         Approach 1 0(2N ) space = 0(N)
+//         int[] hashMap = new int[nums.length + 1];
+        
+//         for(int i = 0; i < nums.length; i++){
+            
+//             hashMap[nums[i]]++;
+            
+//         }
+        
+//         for(int i = 1; i < hashMap.length; i++){
+            
+//             if(hashMap[i] == 0){
+//                 return i;
+//             }
+//         }
+        
+//         return 0;
     }
 }
