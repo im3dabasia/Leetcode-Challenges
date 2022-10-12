942f5237f787997fa9fef8233dc83f3c38e9c2fc
Eshaan Dabasiya <76681468+im3dabasia@users.noreply.github.com>
2022-10-12 23:57:22 +0530

    Time: 3 ms (75.80%), Space: 56.5 MB (33.15%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,17 +1,44 @@
 class Solution {
     public int majorityElement(int[] nums) {
         
-        Arrays.sort(nums);
-        int mid = nums.length/2;
-        for (int i =0 ; i < nums.length - mid; i++ ){
-            
-            if(nums[i] == nums[mid]){
-                return nums[i];
-            }
-            
-        }
-        return 0;
         
+//         Approach 3 Moores Algorithm
+            int cnt = 0;
+            int elem = 0;
+        
+        for (int i = 0 ; i < nums.length; i++){
+            
+            if(cnt == 0) {
+                
+              elem = nums[i];
+  
+            }             
+            if(nums[i] == elem){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
+        }
+        return elem;
+        
+        
+        
+//         Approach 2
+//         Arrays.sort(nums);
+//         int mid = nums.length/2;
+//         for (int i =0 ; i < nums.length - mid; i++ ){
+            
+//             if(nums[i] == nums[mid]){
+//                 return nums[i];
+//             }
+            
+//         }
+//         return 0;
+        
+        
+        
+//         Approach 1
 //         for(int i = 0; i < nums.length; i++){
             
 //             int num = nums[i];
