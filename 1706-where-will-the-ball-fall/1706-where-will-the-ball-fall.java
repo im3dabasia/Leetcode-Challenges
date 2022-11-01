9adf1dce9f5e334e687fc8d7d9549300385290ed
Eshaan Dabasiya <76681468+im3dabasia@users.noreply.github.com>
2022-11-01 12:10:56 +0530

    Time: 2 ms (73.77%), Space: 54 MB (72.75%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] findBall(int[][] grid) {
+        
+        int result[] = new int[grid[0].length];
+        
+        for (int i = 0; i < grid[0].length; i++) {
+            
+            result[i] = findBallDropColumn(0, i, grid);
+        
+        }
+        return result;
+    }
+
+    public int findBallDropColumn(int row, int col, int[][] grid) {
+
+        if (row == grid.length)
+            return col;
+        
+//         next col is the sum 0f curr col + next col can be going to right then +1 else left -1
+        int nextColumn = col + grid[row][col];
+        
+        if (nextColumn < 0 || 
+                nextColumn > grid[0].length - 1 || 
+                grid[row][col] != grid[row][nextColumn]) {
+            return -1;
+        }
+        return findBallDropColumn(row + 1, nextColumn, grid);
+    }
+}
