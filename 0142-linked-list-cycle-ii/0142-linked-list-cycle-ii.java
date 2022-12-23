6d16cd53d9a895b64605b7816a77aea64ecb64df
Eshaan Dabasiya <76681468+im3dabasia@users.noreply.github.com>
2022-12-24 00:05:57 +0530

    Time: 4 ms (42.43%), Space: 43.5 MB (74.80%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,41 +11,73 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        ListNode slowptr = head;
-        ListNode fastptr = head;
-        
-        
         if (head == null || head.next == null){
             return null;
         }
-        
-        
-        while(slowptr != fastptr || fastptr.next != null){
-            slowptr = slowptr.next;
-            if(fastptr.next != null){
 
-                fastptr = fastptr.next.next;
-                
-            }
+        HashMap<ListNode, Integer> map = new HashMap<>();
+        
+        ListNode temp = head;
+        
+        int i = 0;
+        int ans = 0;
+        while(temp != null){
             
-            if(fastptr == null || fastptr.next == null ){
-                return null;
+            if(!map.containsKey(temp)){
+                map.put(temp, i);
             }
-            
-            if(slowptr == fastptr){
+            else{
+                ans =  map.get(temp);
                 break;
             }
+            temp = temp.next;
+            i++;
+            
         }
-        
         ListNode dummy = head;
-        
-
-        while(slowptr!= dummy){
-            
-            slowptr = slowptr.next;
+        for(int k = 0; k < ans; k++){
             dummy = dummy.next;
-            
-        }
-        return slowptr;
+        }  
+        
+        if(temp == null) return null;
+        return dummy;
     }
 }
+
+// Approach 1
+//         ListNode slowptr = head;
+//         ListNode fastptr = head;
+        
+        
+//         if (head == null || head.next == null){
+//             return null;
+//         }
+        
+        
+//         while(slowptr != fastptr || fastptr.next != null){
+//             slowptr = slowptr.next;
+//             if(fastptr.next != null){
+
+//                 fastptr = fastptr.next.next;
+                
+//             }
+            
+//             if(fastptr == null || fastptr.next == null ){
+//                 return null;
+//             }
+            
+//             if(slowptr == fastptr){
+//                 break;
+//             }
+//         }
+        
+//         ListNode dummy = head;
+        
+
+//         while(slowptr!= dummy){
+            
+//             slowptr = slowptr.next;
+//             dummy = dummy.next;
+            
+//         }
+//         return slowptr;
