8ac4820931bcd89be154a6d989d2ac99fc50a7f3
Eshaan Dabasiya <76681468+im3dabasia@users.noreply.github.com>
2023-01-10 14:16:47 +0530

    Time: 460 ms (5.03%), Space: 43.5 MB (62.54%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.java
@@ -0,0 +1,32 @@
+class Solution {
+    List<String> temp;
+    public List<String> letterCasePermutation(String s) {
+        temp = new ArrayList<>();
+        
+        helper(s.toCharArray(), 0);
+        return temp;
+        
+    }
+    private void helper(char[] arr, int index){
+        
+        if(index == arr.length){
+            if(!temp.contains(new String(arr))){
+                temp.add(new String(arr));
+            }
+            return;
+        }
+        
+        if(Character.isDigit(arr[index])){
+            helper(arr, index + 1);
+        }
+        // System.out.println(1 + new String(arr));
+        
+        arr[index]  = Character.toLowerCase(arr[index]);
+        helper(arr, index + 1);
+        arr[index]  = Character.toUpperCase(arr[index]);
+        helper(arr, index + 1);
+    
+        
+    }
+}
+
