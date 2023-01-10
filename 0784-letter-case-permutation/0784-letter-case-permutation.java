81cef8f6c56b12cb1226a2e3870ec8e49f800397
Eshaan Dabasiya <76681468+im3dabasia@users.noreply.github.com>
2023-01-10 14:18:30 +0530

    Time: 1 ms (100.00%), Space: 43.1 MB (74.21%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.java
+++ 0784-letter-case-permutation/0784-letter-case-permutation.java
@@ -20,12 +20,13 @@ class Solution {
             helper(arr, index + 1);
             return;
         }
-        // System.out.println(1 + new String(arr));
-        
+        else{
+            
         arr[index]  = Character.toLowerCase(arr[index]);
         helper(arr, index + 1);
         arr[index]  = Character.toUpperCase(arr[index]);
         helper(arr, index + 1);
+        }
     
         
     }
