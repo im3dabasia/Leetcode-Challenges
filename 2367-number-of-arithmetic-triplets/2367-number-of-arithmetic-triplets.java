5df3b13ba4f9ef4762a35cf37391acba95daccbd
Eshaan Dabasiya <76681468+im3dabasia@users.noreply.github.com>
2023-01-31 09:10:43 +0530

    Time: 635 ms (5.00%), Space: 42.9 MB (5.72%) - LeetHub

---
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int arithmeticTriplets(int[] nums, int diff) {
+        
+        
+        int cnt = 0;
+        int len = nums.length - 1;
+        for(int i = nums.length - 1; i > 0; i--){
+            
+            int top = nums[i];
+            int idx = i - 1;
+            int tempCnt = 1;
+
+            while(idx >= 0  ){
+                System.out.println(top + " " + nums[idx]);
+                
+                if(top - nums[idx] != diff) idx--;
+                else{
+                // System.out.println("kk");
+                    
+                    top = nums[idx];
+                    tempCnt++;
+                    idx--;
+                }
+                
+                System.out.println(  "tempCnt:" + tempCnt);
+                
+                if(tempCnt == 3){
+                    cnt++;
+                    break;
+                } 
+                // System.out.println(top + " " + nums[idx]);
+            } 
+            
+        }
+        return cnt ;
+        
+    }
+}
