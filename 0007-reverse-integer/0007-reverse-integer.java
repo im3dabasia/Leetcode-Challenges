37d214ab594a602dae6f2f0416a49bfba3bedbbd
Eshaan Dabasiya <76681468+im3dabasia@users.noreply.github.com>
2023-02-06 09:27:42 +0530

    Time: 1 ms (100.00%), Space: 39.3 MB (95.42%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -8,8 +8,11 @@ class Solution {
 
             // System.out.println(temp +" " + x + " " + res);
 
-            if(res > Integer.MAX_VALUE/10 || res == Integer.MAX_VALUE/10 && temp > 7) return 0;
-            if(res < Integer.MIN_VALUE/10 || res == Integer.MIN_VALUE/10 && temp < -8) return 0;
+            // if(res > Integer.MAX_VALUE/10 || res == Integer.MAX_VALUE/10 && temp > 7) return 0;
+            // if(res < Integer.MIN_VALUE/10 || res == Integer.MIN_VALUE/10 && temp < -8) return 0;
+            
+            if(res > Integer.MAX_VALUE/10 ) return 0;
+            if(res < Integer.MIN_VALUE/10 ) return 0;
 
             res = res * 10 + temp;
 
