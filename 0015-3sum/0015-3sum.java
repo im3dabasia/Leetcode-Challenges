b880b2cb58a20e65f9be2b9f58466e6a33b14318
Eshaan Dabasiya <76681468+im3dabasia@users.noreply.github.com>
2023-02-24 23:42:21 +0530

    Time: 35 ms (79.09%), Space: 50.2 MB (31.70%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -24,6 +24,8 @@ class Solution {
                         temp.add(nums[low]);
                         temp.add(nums[high]);
 
+                        //Since its sorted and we are already handling duplicated by code below 
+                        // no need to check for duplicates in the res DS. it is complicating the algorithm
                         // if(!res.contains(temp)){
                         //     res.add(temp);
                         // }
@@ -51,43 +53,25 @@ class Solution {
 
 
 // Approach 1
-        
 //         List<List<Integer>> res = new ArrayList<>();
-        
-        
 //         for (int i = 0; i < nums.length -2 ; i++){
-            
 //             for(int j = i + 1;  j < nums.length -1 ; j++){
 //                 List<Integer> temp = new ArrayList<>();
-                
 //                 for(int k = j + 1; k < nums.length; k++){
-                    
 //                     if (nums[i] + nums[j] + nums[k] == 0){
-                        
 //                         temp.add(nums[i]);
 //                         temp.add(nums[j]);
 //                         temp.add(nums[k]);
-                        
 //                         Collections.sort(temp);
-                        
-  
 //                     }
 //                     System.out.println(temp);
-                    
 //                     if (temp.size() != 0 && !res.contains(temp)){
 //                     System.out.println( "222" + temp);
-                        
 //                             res.add(temp);
 //                             // temp.clear();
 //                     }
-                    
 //                     temp.clear();
-                    
-                      
 //                 }
-                
 //             }
-            
 //         }
 //         return res;
-        
