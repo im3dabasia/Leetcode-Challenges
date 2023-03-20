c26e2f964eae2f6729b7056273b96e33aa044527
Eshaan Dabasiya <76681468+im3dabasia@users.noreply.github.com>
2023-03-20 15:21:39 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (37.67%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -5,20 +5,41 @@ class Solution {
             Arrays.fill(dp[i], -1);
         }
         
-        return helper(m - 1,n -1,dp);
+        // return helper(m - 1,n -1,dp);
+        
+        for(int i = 0 ; i < m; i++){
+            
+            for(int j = 0 ; j < n; j++){
+                
+                if(i == 0 && j == 0){
+                    dp[i][j] = 1;
+                    continue;
+                }
+                
+                int up = 0;
+                int left = 0;
+                if(i -1 >= 0)  up = dp[i -1][j];
+                if(j -1 >= 0)  left = dp[i][j -1];
+                dp[i][j] = up + left;
+            }
+        }
+        return dp[m-1][n-1];
         
     }
     
-    private int helper(int r, int c, int[][] dp){
+//     private int helper(int r, int c, int[][] dp){
         
-        if(r == 0 && c == 0) return 1;
-        if(r < 0 || c < 0 )return 0;
+//         if(r == 0 && c == 0) return 1;
+//         if(r < 0 || c < 0 )return 0;
         
-        if(dp[r][c] != -1) return dp[r][c];
+//         if(dp[r][c] != -1) return dp[r][c];
         
-        int up = helper(r - 1, c, dp);
-        int left = helper(r, c - 1, dp);
+//         int up = helper(r - 1, c, dp);
+//         int left = helper(r, c - 1, dp);
         
-        return dp[r][c] =  up + left;
-    }
+//         return dp[r][c] =  up + left;
+//     }
 }
+
+// Time = 0(2^(m * n)) every box will have 2 options to go left and right and there are m * n boxes
+// Space = 0(dp array + recursion stack space)
