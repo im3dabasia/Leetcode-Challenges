a94b78aa206ddb53e843eab7bb5139705e0d717f
Eshaan Dabasiya <76681468+im3dabasia@users.noreply.github.com>
2023-03-21 17:07:29 +0530

    Time: 106 ms (5.06%), Space: 42.7 MB (91.44%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] arr, int n) {
+        
+        int cnt = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            
+            boolean emptyLeft = (i == 0) || arr[i - 1] == 0;
+            boolean emptyRight = (i == arr.length - 1) || arr[i + 1] == 0;
+            
+            
+            if(emptyLeft && emptyRight){
+                System.out.println(i);
+                if(arr[i] != 1) cnt++;
+                
+                arr[i] = 1;
+                
+                // cnt++;
+            }
+            // System.out.println(Arrays.toString(arr));
+            
+        }
+        System.out.println(cnt);
+        return cnt >= n;
+    }
+}
