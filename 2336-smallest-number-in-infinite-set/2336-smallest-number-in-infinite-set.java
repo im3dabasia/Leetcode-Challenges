500c89b350b6f8703cf330e2e3bd436193d1ab29
Eshaan Dabasiya <76681468+im3dabasia@users.noreply.github.com>
2023-05-06 00:28:17 +0530

    Time: 12 ms (51.85%), Space: 43.4 MB (33.61%) - LeetHub

---
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.java
@@ -0,0 +1,31 @@
+class SmallestInfiniteSet {
+    private SortedSet<Integer> addedIntegers;
+    private Integer currentInteger;
+    
+    public SmallestInfiniteSet() {
+        addedIntegers = new TreeSet<>();
+        currentInteger = 1;
+    }
+    
+    public int popSmallest() {
+        int answer;
+
+        if (!addedIntegers.isEmpty()) {
+            answer = addedIntegers.first();
+            addedIntegers.remove(answer);
+        } 
+
+        else {
+            answer = currentInteger;
+            currentInteger += 1;
+        }
+        return answer;
+    }
+    
+    public void addBack(int num) {
+        if (currentInteger <= num || addedIntegers.contains(num)) {
+            return;
+        }
+        addedIntegers.add(num);
+    }
+}
