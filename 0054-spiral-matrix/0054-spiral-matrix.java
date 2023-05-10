44ee111fe5a8fa3c541b187676da6cd9f0b435d6
Eshaan Dabasiya <76681468+im3dabasia@users.noreply.github.com>
2023-05-10 14:01:16 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (7.93%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,40 +1,62 @@
 class Solution {
-    public List<Integer> spiralOrder(int[][] arr) {
-        List<Integer> res = new ArrayList<Integer>();
-
-        int minR =0;
-        int minC=0;
-        int maxR= arr.length-1;
-        int maxC = arr[0].length-1;
-        int t =arr[0].length*arr.length;
-        int count=0;
-        while(count<t){
+    public List<Integer> spiralOrder(int[][] matrix) {
+        
+        int minR = 0;
+        int minC = 0;
+        
+        int maxR = matrix.length -1;
+        int maxC = matrix[0].length - 1;
+        int total = (maxR+1) * (1+maxC);
+        
+        int count = 0;
+        
+        List<Integer> res = new ArrayList<>();
+        
+        while(count < total){
+            
             //top
-            for(int i =minC;i<=maxC && count<t ;i++){
-                res.add(arr[minR][i]);
+            for(int i = minC; i <= maxC && count < total; i++){
+                res.add(matrix[minR][i]);
                 count++;
+
             }
             minR++;
+            // System.out.println("1" + res + " " + minR + " " + maxR);
 
             //right
-            for(int i =minR;i<=maxR && count<t;i++){
-                res.add(arr[i][maxC]);
+            for(int i = minR; i <= maxR && count < total; i++){                
+                res.add(matrix[i][maxC]);
                 count++;
+
             }
             maxC--;
-            //bottom
-            for(int i = maxC;i>=minC&&count<t;i--){
-                res.add(arr[maxR][i]);
+            // System.out.println("2" + res);
+
+            //bottom        
+            // System.out.println("hello" + maxR + " " + maxC );
+            
+            for(int i = maxC ; i >= minC && count < total; i--){
+            // System.out.println(matrix[maxR][i] );
+                
+                res.add(matrix[maxR][i ] );
                 count++;
+
             }
             maxR--;
-            //left 
-            for(int i=maxR;i>=minR && count<t;i--){
-                res.add(arr[i][minC]);
+            // System.out.println("3" + res);
+            
+
+            //left
+            for(int i = maxR ; i >= minR && count < total; i--){
+                res.add(matrix[i][minC]);
                 count++;
+
             }
             minC++;
+            // System.out.println("4" + res);
+            
         }
+            
         return res;
     }
 }
