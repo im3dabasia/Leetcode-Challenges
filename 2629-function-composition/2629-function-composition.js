5949bf0d026ac76cfe72b632100f794346fd89f5
Eshaan Dabasiya <76681468+im3dabasia@users.noreply.github.com>
2023-05-13 14:28:35 +0530

    Time: 135 ms (5.20%), Space: 48.2 MB (5.33%) - LeetHub

---
+++ 2629-function-composition/2629-function-composition.js
@@ -0,0 +1,27 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+	return function(x) {
+        let tmp = x;
+        let ans = 0;
+        for(let i = functions.length -1; i >= 0; i--){
+            console.log(functions[i].toString())
+            ans =  functions[i](tmp)
+            console.log(ans);
+            tmp = ans;
+            console.log(tmp);
+            
+        }
+        
+        if(functions.length == 0 ) return x;
+        return ans;
+    }
+
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
