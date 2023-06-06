23b12bcc1f97f9fdeafd191006958686564f40a2
Eshaan Dabasiya <76681468+im3dabasia@users.noreply.github.com>
2023-06-06 09:51:12 +0530

    Time: 2 ms (93.76%), Space: 61.2 MB (20.46%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,39 +1,12 @@
-// /class Solution {
-//     public int maxProfit(int[] prices) {
-//         int profit = 0;
-//         int min = Integer.MAX_VALUE;
-        
-//         for(int i = 0; i < prices.length; i++){
-            
-//             if(prices[i] < min){
-//                 min = prices[i];
-                
-//             }
-//             else{
-//                 if(profit < prices[i] - min){
-//                     profit = prices[i] - min;
-                    
-//                 }
-//             }
-//         }
-//         return profit;
-//     }
-// }
-
 class Solution {
-    public int maxProfit(int[] prices) {
-        int profit = 0;
-        int min = Integer.MAX_VALUE;
-        
-        for(int i = 0; i < prices.length; i++){
-            
-            if(prices[i] < min ){
-                min = prices[i];
-            }
-            else if (profit < prices[i] - min){
-                profit = prices[i] - min;
-            }
-        }
-        return profit;
+    public int maxProfit(int[] arr) {
+    int maxPro = 0;
+    int minPrice = Integer.MAX_VALUE;
+    for (int i = 0; i < arr.length; i++) {
+        minPrice = Math.min(minPrice, arr[i]);
+        maxPro = Math.max(maxPro, arr[i] - minPrice);
+    }
+    return maxPro;
+
     }
 }
