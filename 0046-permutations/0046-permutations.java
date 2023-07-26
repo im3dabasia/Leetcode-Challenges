ca516507ae8c9e13e2073b1de071f082f23baf26
Eshaan Dabasiya <76681468+im3dabasia@users.noreply.github.com>
2023-07-26 11:21:50 +0530

    Time: 2 ms (48.55%), Space: 44.1 MB (47.73%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,23 +1,28 @@
 class Solution {
-public List<List<Integer>> permute(int[] nums) {
-   List<List<Integer>> list = new ArrayList<>();
-   // Arrays.sort(nums); // not necessary
-   backtrack(list, new ArrayList<>(), nums);
-   return list;
-}
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        
+        backtracker(res, temp, nums);
+        return res;
+        
+    }
+    private void backtracker(List<List<Integer>> res, List<Integer> temp, int[] nums){
+        
+        if(temp.size() == nums.length){
+            
+            res.add(new ArrayList<>(temp));
+        }
+        else{
+        
+            for(int i = 0 ; i < nums.length; i++){
+                if(temp.contains(nums[i])) continue;
 
-private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
-   if(tempList.size() == nums.length){
-      list.add(new ArrayList<>(tempList));
-   } else{
-      for(int i = 0; i < nums.length; i++){ 
-         if(tempList.contains(nums[i])) continue; // element already exists, skip
-         tempList.add(nums[i]);
-         backtrack(list, tempList, nums);
-         tempList.remove(tempList.size() - 1);
-      }
-   }
-} 
-
-
+                temp.add(nums[i]);
+                backtracker(res, temp, nums);
+                temp.remove(temp.size() - 1);
+            }    
+        }
+        
+    }
 }
